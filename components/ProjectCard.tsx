18741e8a3b9b70e53f57afabbb5a4a65d2bd63f1
chriskolb00 <cmlkolb@gmail.com>
2023-12-11 22:19:46 -0600

    fixed img src

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 interface Props{
-    img:any;
+    img:string;
     title:string;
     link:string;
 }
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,30 +1,26 @@
 import React from 'react'
 import Title from './Title'
 import {FaProjectDiagram} from 'react-icons/fa' 
-import ProjectCard from './ProjectCard'
-import p1 from '../public/images/weather.png'
-import p2 from '../public/images/imageGen.png'
-import p3 from '../public/images/thread.png'
-import p4 from '../public/images/youtube.png'
+import ProjectCard from './ProjectCard' 
 const Projects = () => {
   return (
     <div className='wrapper '>
         <Title text="Projects" icon={<FaProjectDiagram/>} />
         <div className='grid grid-cols-1 md:grid-cols-2 gap-10 place-items-center'>
             <ProjectCard 
-            img={p1}
+            img="/images/weather.png"
             title="Weather App"
             link="https://weather-app-react-chriskolb00.vercel.app/"/>
             <ProjectCard 
-            img={p2}
+            img="/images/imageGen.png"
             title="Photo Generator"
             link="https://photo-app-react-chriskolb00.vercel.app/"/>
             <ProjectCard 
-            img={p3}
+            img="/images/thread.png"
             title="Thread Clone"
             link="https://thread-app-chriskolb00.vercel.app/"/>
             <ProjectCard 
-            img={p4}
+            img="/images/youtube.png"
             title="Youtube Clone"
             link="https://youtube-clone-ck.netlify.app/"/>
         </div>
